852648cbcf6641c5c9dabd4f77313fbbcc5fd6f5
Vasi Logeswaran <Vasi.Logeswaran@gmail.com>
2018-07-24 14:49:16 +0100

    A Thread Can Have Replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,5 +12,22 @@
             </div>
         </div>
     </div>
+    <hr>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach ($thread->replies as $reply)
+                <div class="card">
+                    <div class="card-header">
+                        <a href=""> {{ $reply->owner->name }} </a>
+                         said {{ $reply->created_at->diffForHumans() }} ...
+                    </div>
+                    <div class="card-body">
+                        <div class="body">{{ $reply->body }}</div>
+                    </div>
+                </div>
+            @endforeach
+            <hr>
+        </div>
+    </div>
 </div>
 @endsection
