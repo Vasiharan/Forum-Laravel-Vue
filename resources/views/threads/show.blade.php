f4ac6dd243be95d849191ab902ad5f791f37d05e
Vasi Logeswaran <Vasi.Logeswaran@gmail.com>
2018-07-25 13:59:26 +0100

    Thread view refactor Thread creator

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ $thread->title }}</div>
+                <div class="card-header">
+                <a href=""> {{ $thread->creator->name }} </a> posted:
+                {{ $thread->title }}
+                </div>
                 <div class="card-body">
                     <div class="body">{{ $thread->body }}</div>
                 </div>
@@ -16,15 +19,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @foreach ($thread->replies as $reply)
-                <div class="card">
-                    <div class="card-header">
-                        <a href=""> {{ $reply->owner->name }} </a>
-                         said {{ $reply->created_at->diffForHumans() }} ...
-                    </div>
-                    <div class="card-body">
-                        <div class="body">{{ $reply->body }}</div>
-                    </div>
-                </div>
+                @include('threads.reply')
             @endforeach
             <hr>
         </div>
