a06529f0789ad8dad5c473aeb17e72033288361b
Vasi Logeswaran <Vasi.Logeswaran@gmail.com>
2018-07-31 10:19:19 +0100

    - Create thread form - Exception Handling test refactor

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -10,6 +10,18 @@ class CreateThreadsTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    function guests_may_not_create_threads()
+    {
+        $this->withExceptionHandling();
+
+        $this->get('/threads/create')
+            ->assertRedirect('/login');
+            
+        $this->post('/threads', [])
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
     public function guest_cannot_create_thread()
     {    
         $this->expectException('Illuminate\Auth\AuthenticationException');  
