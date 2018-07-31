354ee6fa6d65bc43d3d6903f731b35b378b3191f
Vasi Logeswaran <Vasi.Logeswaran@gmail.com>
2018-07-31 15:31:04 +0100

    - Thread belongs to a channel test

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -30,14 +30,22 @@ class ThreadTest extends TestCase
         $this->assertInstanceOf('App\User', $this->thread->creator);
     }
 
-     /** @test */
-     public function a_thread_can_add_a_reply()
-     {
-         $this->thread->addReply([
+    /** @test */
+    public function a_thread_can_add_a_reply()
+    {
+        $this->thread->addReply([
              'body' => 'Foobar',
-             'user_id' => 1
+             'user_id' => 1,
          ]);
 
-         $this->assertCount(1, $this->thread->replies);
-     }
+        $this->assertCount(1, $this->thread->replies);
+    }
+
+    /** @test */
+    public function a_thread_belongs_to_a_channel()
+    {
+        $thread = create('App\Thread');
+
+        $this->assertInstanceOf('App\Channel', $thread->channel);
+    }
 }
